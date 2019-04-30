731272578b766a6aa42dcdde3cffa3d818c2b573
Lee Meng Fong <28826976+mengfong96@users.noreply.github.com>
2019-04-30 22:17:54 +0800

    Initial Commit

---
+++ contracts/Election.sol
@@ -0,0 +1,59 @@
+pragma solidity ^0.5.0;
+
+contract Election {
+    // Model a Candidate
+    struct Candidate {
+        uint id;
+        string name;
+        uint voteCount;
+    }
+
+    // Store accounts that have voted
+    mapping(address => bool) public voters;
+    // Store Candidates
+    // Fetch Candidate
+    mapping(uint => Candidate) public candidates;
+    // Store Candidates Count
+    uint public candidatesCount;
+
+    // voted event
+    event votedEvent (
+        uint indexed _candidateId
+    );
+
+    constructor() public {
+        addCandidate("Daniel Soon Yuen Hao");
+        addCandidate("Nur Iman Aqilah");
+        addCandidate("Sasha Shamsuddin Lee");
+        addCandidate("Mohammad Syazwan");
+        addCandidate("Nurin Izzati binti Noor Azman");
+        addCandidate("Abdul Wahab bin Hosni");
+        addCandidate("Abdullah Hakim");
+        addCandidate("Coleman");
+        addCandidate("Nichole");
+        addCandidate("Fran");
+
+    }
+
+    function addCandidate (string memory _name) public {
+        candidatesCount ++;
+        candidates[candidatesCount] = Candidate(candidatesCount, _name, 0);
+    }
+
+    function vote (uint _candidateId) public {
+        // require that they haven't voted before
+        require(!voters[msg.sender]);
+
+        // require a valid candidate
+        require(_candidateId > 0 && _candidateId <= candidatesCount);
+
+        // record that voter has voted
+        voters[msg.sender] = true;
+
+        // update candidate vote Count
+        candidates[_candidateId].voteCount ++;
+
+        // trigger voted event
+        emit votedEvent(_candidateId);
+    }
+}
