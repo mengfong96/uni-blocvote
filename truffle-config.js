4c73fc7b449f56ce0c56cd9ff9d661af3dc38e32
mengfong <leemengfong.96@gmail.com>
2022-02-03 17:18:55 +0800

    Dependency update/remove

--- truffle-config.js
+++ truffle-config.js
@@ -1,18 +1,14 @@
-var HDWalletProvider = require("truffle-hdwallet-provider");
-var mnemonic = "pull truly talent canyon subway enhance master romance badge region wage asthma domain walk scale";
-
 module.exports = {
+  // See <http://truffleframework.com/docs/advanced/configuration>
+  // for more about customizing your Truffle configuration!
   networks: {
     development: {
       host: "127.0.0.1",
       port: 8545,
-      network_id: "*"
+      network_id: "*" // Match any network id
     },
-    "ropsten-infura": {
-      provider: () => new HDWalletProvider(mnemonic, "https://ropsten.infura.io/v3/ed89e9fa1e2e46428400fb28fd2307cb"),
-      network_id: 3,
-      gas: 4700000,
-      gasPrice: 100000000000
+    develop: {
+      port: 8545
     }
   }
 };
